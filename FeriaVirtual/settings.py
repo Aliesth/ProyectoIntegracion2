d6bd53192720f0a3918d232a04a093b3b049d44e
Edgar Sanhueza <edgar.sanhueza.r@gmail.com>
2025-11-29 17:20:31 -0300

    feat: Final config Render y requisitos

--- FeriaVirtual/settings.py
+++ FeriaVirtual/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 from django.contrib.messages import constants as messages
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -93,7 +93,11 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600, 
+        ssl_require=True
+    )
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
