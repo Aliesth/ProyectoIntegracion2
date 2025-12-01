1f3d20cf79ad22b43b2f5b2df327bf6457f5770c
Edgar Sanhueza <edgar.sanhueza.r@gmail.com>
2025-12-01 19:33:50 -0300

    Fix: sincronizar migraciones cloudinary

---
+++ aplicacion/migrations/0015_fix_cloudinary_imagen.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('aplicacion', '0014_alter_fruta_options_fruta_imagen_and_more'),
+    ]
+
+    operations = [
+        # Vacío a propósito – Solo sincroniza el historial en Render
+    ]
