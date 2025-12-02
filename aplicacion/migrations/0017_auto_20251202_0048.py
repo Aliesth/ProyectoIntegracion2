a0081df4279c78b12a719ee2274b9c6d35ea6585
Edgar Sanhueza <edgar.sanhueza.r@gmail.com>
2025-12-02 01:05:58 -0300

    Agrego fixture superusuario

--- aplicacion/migrations/0017_auto_20251202_0048.py
+++ aplicacion/migrations/0017_auto_20251202_0048.py
@@ -6,13 +6,4 @@ class Migration(migrations.Migration):
         ('aplicacion', '0016_alter_fruta_options_fruta_imagen_and_more'),  # Ajusta según tu última migración aplicada
     ]
 
-    operations = [
-        # Ejemplo: agregar un nuevo campo
-      
-        # Ejemplo: modificar un campo existente
-        migrations.AddField(
-            model_name='pedido',
-            name='tipo_envio',
-            field=models.CharField(choices=[('Nacional', 'Nacional'), ('Internacional', 'Internacional')], default='Nacional', max_length=15, verbose_name='Tipo de Envío'),  # permite valores nulos si antes no los permitía
-        ),
-    ]
+    
