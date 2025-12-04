b5948aa3592385082dcadf9acce15d3c96667708
Edgar Sanhueza <edgar.sanhueza.r@gmail.com>
2025-12-04 19:44:21 -0300

    Agregar tabla Pedidos

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -494,13 +494,12 @@ def vaciar_tablas_pedido(request):
     
     # Se eliminan los registros de Pedido. El borrado en cascada 
     # se encargará de DetallePedido.
-    #count_pedidos = Pedidos.objects.all().delete()
-    count_pedidos = Pedidos.objects.all()
-    print(count_pedidos)
+    count_pedidos = Pedido.objects.all().delete()
+    
     # El resultado de .delete() es una tupla: (número_objetos_eliminados, {diccionario_de_counts})
     total_eliminado = count_pedidos[0]
     
-    mensaje = (f"{count_pedidos}"
+    mensaje = (
         f"🗑️ *¡Éxito!* La limpieza ha sido ejecutada."
         f"Se eliminaron *{total_eliminado}* registros de Pedido y sus líneas de detalle relacionadas."
         f"<br><br>Intente acceder a la tabla Pedido en el admin nuevamente."
