6ad7bd93c038d0b2a34b297dfd42ed981d2cf5f4
Edgar Sanhueza <edgar.sanhueza.r@gmail.com>
2025-12-04 23:14:25 -0300

    Agregar tabla Pedidos

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Pedido, DetallePedido, Fruta, Contrato, Perfil
+from .models import Pedido, DetallePedido, Fruta, Contrato, Perfil,PedidosRealizados
 
 admin.site.register(Contrato)
+admin.site.register(PedidosRealizados)
 admin.site.register(Pedido)
 admin.site.register(DetallePedido)
 admin.site.register(Fruta)
