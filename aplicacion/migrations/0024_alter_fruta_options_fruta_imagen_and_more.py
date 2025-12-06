ce52edffddde7a344b8583052da92830a1bd5524
Edgar Sanhueza <edgar.sanhueza.r@gmail.com>
2025-12-06 00:51:52 -0300

    Agregar tabla Pedidos

--- aplicacion/migrations/0024_alter_fruta_options_fruta_imagen_and_more.py
+++ aplicacion/migrations/0024_alter_fruta_options_fruta_imagen_and_more.py
@@ -14,10 +14,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        
-        migrations.AlterField(
-            model_name='detallepedido',
-            name='pedido',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='detalles', to='aplicacion.PedidosRealizados'),
-        ),
+    
     ]
